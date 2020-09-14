330cf48e9a06299f1af1acdec4848be2708e602b
Evulpes <54101182+Evulpes@users.noreply.github.com>
2020-09-15 00:17:37 +0100

    Started adding counter anticheat.

--- NativeMethods.cs
+++ NativeMethods.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Runtime.InteropServices;
 using System.Text;
 
@@ -9,16 +10,54 @@ namespace The_Possible_Game
     {
         public static class Memoryapi
         {
+            //move me
+            [DllImport("kernel32.dll")]
+            public static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, IntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, IntPtr lpThreadId);
+
             [DllImport("kernel32.dll", SetLastError = true)]
             public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out IntPtr lpNumberOfBytesRead);
 
+            [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+            public static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, Winnt.AllocationType flAllocationType, Winnt.MemoryProtection flProtect);
+
             [DllImport("kernel32.dll", SetLastError = true)]
             public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int nSize, out IntPtr lpNumberOfBytesWritten);
+
         }
         public static class Processthreadsapi
         {
             [DllImport("Kernel32", SetLastError = true)]
-            public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int processId);
+            public static extern IntPtr OpenProcess(Winnt.ProcessAccessFlags dwDesiredAccess, bool bInheritHandle, int processId);
+        }
+        public static class Winnt
+        {
+            public enum AllocationType
+            {
+                MEM_COMMIT = 0x1000,
+                MEM_FREE = 0x10000,
+                MEM_RESERVE = 0x2000,
+            }
+            public enum ProcessAccessFlags
+            {
+                PROCESS_ALL_ACCESS = 0xFFFF,
+            }
+            public enum MemoryProtection : uint
+            {
+                PAGE_EXECUTE = 0x10,
+                PAGE_EXECUTE_READ = 0x20,
+                PAGE_EXECUTE_READWRITE = 0x40,
+                PAGE_EXECUTE_WRITECOPY = 0x80,
+                PAGE_NOACCESS = 0x01,
+                PAGE_READONLY = 0x02,
+                PAGE_READWRITE = 0x04,
+                PAGE_WRITECOPY = 0x08,
+                PAGE_TARGETS_INVALID = 0x40000000,
+                PAGE_TARGETS_NO_UPDATE = 0x40000000,
+                PAGE_GUARD = 0x100,
+                PAGE_NOCACHE = 0x200,
+                PAGE_WRITECOMBINE = 0x400,
+
+            }
         }
 
     }
--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Runtime.InteropServices;
 //See https://github.com/Evulpes/Generic-Bytescan-Library
 using Generic_Bytescan_Library;
@@ -10,7 +11,7 @@ namespace The_Possible_Game
     class Program
     {
 
-        
+        private static readonly uint IsDebuggerPresentByte = 0x8F5000;
         /// <summary>
         /// An array of 5 NOP opcodes.
         /// </summary>
@@ -23,6 +24,12 @@ namespace The_Possible_Game
 
         static void Main()
         {
+            Console.WriteLine("The Impossible Game Launcher");
+
+            AntiAntiCheat.StartAntiDebuggingPatchThread();
+
+
+
             Console.WriteLine("Attempting to patch The Impossible Game.");
 
             Process p = Process.GetProcessesByName("ImpossibleGame").FirstOrDefault();
@@ -30,7 +37,7 @@ namespace The_Possible_Game
             if (p == null)
                 throw new Exception("Impossible Game not running");
 
-            IntPtr handle = NativeMethods.Processthreadsapi.OpenProcess(0x001F0FFF, false, p.Id);
+            IntPtr handle = NativeMethods.Processthreadsapi.OpenProcess(NativeMethods.Winnt.ProcessAccessFlags.PROCESS_ALL_ACCESS, false, p.Id);
 
             if (handle == IntPtr.Zero)
                 throw new Exception("Invalid Handle");
@@ -62,6 +69,62 @@ namespace The_Possible_Game
 
             Console.WriteLine("Patched. Press any key to continue");
             Console.ReadKey();
+        }
+    }
+    class AntiAntiCheat
+    {
+        private static readonly byte[] isDebuggerPresentByteCheck = new byte[]
+        {
+            0x64, 0xa1, 0x30, 0x00, 0x00, 0x00,         //mov eax, dword ptr fs:[30]
+            0x80, 0x78, 0x2, 0x1,                       //cmp byte ptr [eax+2], 0x1
+            0x75, 0xFA,                                 //jne 0xFFFFFFC
+            0xc6, 0x40, 0x02, 0x00,                     //mov byte ptr [eax+2], 0x0
+            0xeb, 0xf2                                  //jmp 0xfffffff4
+        };
+        public static int StartAntiDebuggingPatchThread()
+        {
+            int tigPid;
+            try
+            {
+                tigPid = Process.GetProcessesByName("ImpossibleGame")[0].Id;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                return (int)CustomErrors.PROCESS_NOT_RUNNING;
+            }
+
+            IntPtr handle = NativeMethods.Processthreadsapi.OpenProcess(NativeMethods.Winnt.ProcessAccessFlags.PROCESS_ALL_ACCESS, false, tigPid);
+            if (handle == IntPtr.Zero)
+                return Marshal.GetLastWin32Error();
+
+            IntPtr remoteThreadAddr = NativeMethods.Memoryapi.VirtualAllocEx
+            (
+                handle, IntPtr.Zero, (uint)isDebuggerPresentByteCheck.Length, 
+                NativeMethods.Winnt.AllocationType.MEM_COMMIT, 
+                NativeMethods.Winnt.MemoryProtection.PAGE_EXECUTE_READWRITE
+            );
+            if (remoteThreadAddr == IntPtr.Zero)
+                return Marshal.GetLastWin32Error();
+            
+            if (!NativeMethods.Memoryapi.WriteProcessMemory(handle, remoteThreadAddr, isDebuggerPresentByteCheck, isDebuggerPresentByteCheck.Length, out _))
+                return Marshal.GetLastWin32Error();
+
+            IntPtr remoteThreadHandle = NativeMethods.Memoryapi.CreateRemoteThread
+            (
+                handle, IntPtr.Zero, 0, remoteThreadAddr, 
+                IntPtr.Zero, 0, IntPtr.Zero
+            );
+
+            if (remoteThreadHandle == IntPtr.Zero)
+                return Marshal.GetLastWin32Error();
+
+            return 0;
+        }
+        public enum CustomErrors
+        {
+            #region custom
+            PROCESS_NOT_RUNNING = 16000,
+            #endregion
         }
     }
 }
